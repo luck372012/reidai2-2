95fce3d5f64c79522063f1e792fa1cffdd6334e6
luck372012 <luck372012@gmail.com>
2021-05-25 11:42:35 +0900

    課題1 修正2

--- kadai1.py
+++ kadai1.py
@@ -5,6 +5,7 @@
 source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
 ### 検索ツール
+import csv
 def search():
     word =input("鬼滅の登場人物の名前を入力してください >>> ")
   
@@ -14,18 +15,17 @@ def search():
     print("{}が見つかりません".format(word))
     source.append(word)
     print(source)
-        
-if __name__ == "__main__":
-    search()
-
+  
 # dataを読み込み
-with open("./data.csv") as f:
-    l_strip = [s.strip() for s in f.readlines()]
-    source.append(l_strip)
-    print(l_strip)
+    with open("./data.csv") as f:
+       l_strip = [s.strip() for s in f.readlines()]
+       source.append(l_strip)
+       print(l_strip)
 
 # data1csvに書き込み
-import csv
-with open("utf8.data1.csv", mode="w", encoding='utf8') as f:
-    writer = csv.writer(f)
-    writer.writerow(source)
+    with open("utf8.data1.csv", mode="w", encoding='utf8') as f:
+       writer = csv.writer(f)
+       writer.writerow(source)
+      
+if __name__ == "__main__":
+     search()    
