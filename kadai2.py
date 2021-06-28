3ab8972176523a55b7a787bd69d7c95eaac568e6
luck372012 <luck372012@gmail.com>
2021-06-28 21:22:02 +0900

    5回目

--- kadai2.py
+++ kadai2.py
@@ -62,7 +62,7 @@ def main():
     exp_name_list = []
    
     # 検索結果の一番上の会社名を取得
-    name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+    name_list = driver.find_elements_by_css_selector(".cassetteRecruit .cassetteRecruit__name")
 
 
     # 1ページ分繰り返し
@@ -77,78 +77,79 @@ def main():
     tables=[]
     trs =[] 
 
-    tables = driver.find_elements_by_css_selector("div.cassetteRecruit__main ")
+    tables = driver.find_elements_by_css_selector(".cassetteRecruit .tableCondition")
     print(len(tables))
 
     for table in tables:
-        for tr in trs:
-            trs = table.find_elements_by_css_selector("tr")
-            print(len(trs))
-
-
-
-
-
-    work_list = trs[2].find_elements_by_css_selector("td")
-    sarary_list = trs[4].find_elements_by_css_selector("td")
-
-    # 取得した要素を1つずつ表示
-    print(len(work_list))
-    for work in work_list:   
+        trs = table.find_elements_by_css_selector("tr")
+        work = trs[2].find_element_by_css_selector("td")
+        sarary = trs[3].find_element_by_css_selector("td")
         exp_work_list.append(work.text) 
-        print(work.text)
-     
-
-    print(len(sarary_list))
-    for sarary in sarary_list:   
         exp_sarary_list.append(sarary.text) 
-        print(sarary.text)    
-
 
 
     # 次のページクリック   
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    driver.find_element_by_class_name("pager__next").find_element_by_tag_name("a").click()
+    #driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # driver.find_element_by_class_name("pager__next").find_element_by_tag_name("a").click()
+    while True:
+        try:
+            next_page_link = driver.find_element_by_css_selector(".iconFont--arrowLeft").get_attribute("href")
+            driver.get(next_page_link)
+
+            tables = driver.find_elements_by_css_selector(".cassetteRecruit .tableCondition")
+            print(len(tables))
+
+            for table in tables:
+                trs = table.find_elements_by_css_selector("tr")
+                work = trs[2].find_element_by_css_selector("td")
+                sarary = trs[3].find_element_by_css_selector("td")
+                exp_work_list.append(work.text) 
+                exp_sarary_list.append(sarary.text) 
 
 
-    #２ページ目会社情報取得
-    name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+        except:
+            break
+    # Comment:2ページ目以降は、上記処理をwhileループにて繰り返すことで実現する
+    # #２ページ目会社情報取得
+    # name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
 
 
-    # 1ページ分繰り返し
-    print(len(name_list))
-    for name in name_list:
-        exp_name_list.append(name.text)
-        print(name.text)
+    # # 1ページ分繰り返し
+    # print(len(name_list))
+    # for name in name_list:
+    #     exp_name_list.append(name.text)
+    #     print(name.text)
 
 
 
-    tables = driver.find_elements_by_css_selector("div.cassetteRecruit__main")
-    for table in tables:
-        for tr in trs:
-            trs = table.find_elements_by_css_selector("tr")
+    # tables = driver.find_elements_by_css_selector("div.cassetteRecruit__main")
+    # for table in tables:
+    #     for tr in trs:
+    #         trs = table.find_elements_by_css_selector("tr")
   
-    work_list = trs[2].find_elements_by_css_selector("td")
-    sarary_list = trs[4].find_elements_by_css_selector("td")
+    # work_list = trs[2].find_elements_by_css_selector("td")
+    # sarary_list = trs[4].find_elements_by_css_selector("td")
 
-    # 取得した要素を1つずつ表示
-    print(len(work_list))
-    for work in work_list:   
-        exp_work_list.append(work.text) 
-        print(work.text)
+    # # 取得した要素を1つずつ表示
+    # print(len(work_list))
+    # for work in work_list:   
+    #     exp_work_list.append(work.text) 
+    #     print(work.text)
     
-    print(len(sarary_list))
-    for sarary in sarary_list:   
-        exp_sarary_list.append(sarary.text) 
-        print(sarary.text)       
+    # print(len(sarary_list))
+    # for sarary in sarary_list:   
+    #     exp_sarary_list.append(sarary.text) 
+    #     print(sarary.text)       
 
 
 
     # CSVファイルに出力
-    d = {'会社名': exp_name_list, '勤務先': work_list, '年収': sarary_list}
-    df = pd.DataFrame(d.values(), index=d.keys()).T
+    df = pd.DataFrame({'会社名': exp_name_list, 
+                       '勤務先': exp_work_list, 
+                       '年収': exp_sarary_list})
     print(df)
-    df.to_csv("utf8.data1.csv","a") 
+    df.to_csv("utf8.data1.csv", mode="w", index=False) 
+    driver.quit()
    
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
