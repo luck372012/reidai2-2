1e04b0e4e957f9977c00ff448e054fac8b0d878f
luck372012 <luck372012@gmail.com>
2021-08-10 12:59:07 +0900

    2回目

--- ullet.py
+++ ullet.py
@@ -48,6 +48,10 @@ df = pd.DataFrame()
 names=[]
 links=[]
 
+sales=[] 
+incomes=[] 
+assets=[] 
+
 #Googleスプレッドシートの初期化
 SCOPES = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 #CRED = ServiceAccountCredentials.from_json_keyfile_name('sheet_key_prod.json', SCOPES)  #PROD
@@ -59,46 +63,68 @@ workbook = gc.open_by_key('1_Ghg0HTIvNf_HExc6KWMrwe7-pZoqyg7LfkAuDkJQXo')
 worksheet = gc.open("ullet").get_worksheet(1)
 
 
-
 # main処理
 url = "http://www.ullet.com/search.html#page/{}"
 
-for i in range(1,4):
+for i in range(1,2):
     target_url= url.format(i) 
-    print(target_url)
+    #print(target_url)
     sleep(1)
     driver = webdriver.Chrome('chromedriver',options=options)
     driver.get(target_url)
+
 
 #会社名を取得してくる
     table = driver.find_elements_by_tag_name("table")[1] 
     class_names=table.find_elements_by_class_name("company_name")
     for name in class_names:
-        print(name.get_attribute("href"))
+        #print(name.get_attribute("href"))
         links.append(name.get_attribute("href"))    
-        print(links)
+        #print(links)
      
       
     for link in links: 
         driver.get(link)
-        name=driver.find_element_by_id("company_name0")
-        print(name.text)
+        name= driver.find_element_by_id("company_name0")
+        #print(name.text)
+
+        table = driver.find_elements_by_tag_name("table")[0] 
+        trs = table.find_elements_by_tag_name("tr")
+      
+        #売上高を出力する
+        sales = trs[1].find_elements_by_tag_name("td")[0] 
+        sales.text.split("兆円")
+        #print((float(sales.text.split("兆円")[0]))*1000000000000)
+        sales=(str((float(sales.text.split("兆円")[0]))*1000000000000))
+       
+        #純利益を出力する
+        incomes = trs[1].find_elements_by_tag_name("td")[1] 
+        #print(incomes.text)
+        if "兆円" in incomes.text:
+            incomes.text.split("兆円")
+            incomes=(str((float(incomes.text.split("兆円")[0]))*1000000000000))
+
+        elif "億円" in incomes.text:    
+            incomes.text.split("億円")
+            incomes=(str((float(incomes.text.split("億円")[0]))*100000000))
 
 
-
+        #総資産を出力する
+        assets = trs[1].find_elements_by_tag_name("td")[3] 
+        assets.text.split("兆円")
+        assets=(str((float(assets.text.split("兆円")[0]))*1000000000000))
 
         df = df.append(
-                    {'会社名': name.text}, ignore_index=True) 
+                    {'会社名': name.text, '売上高': sales, '純利益': incomes,
+                     '総資産': assets}, ignore_index=True) 
 
-
-
+print(df) 
 
 #CSV出力
-#df.to_csv("data1.csv", mode="w", index=False, encoding='cp932') 
+df.to_csv("data1.csv", mode="w", index=False, encoding='cp932') 
 #excel出力
-#df.to_excel('data1.xlsx', sheet_name='ullet', index=False)
+df.to_excel('data1.xlsx', sheet_name='ullet', index=False)
 
-#スプレッドシート出力
-#print(df)      
+#スプレッドシート出力     
 update_sheet = workbook.worksheet("シート1")
 set_with_dataframe(update_sheet, df,resize=True, include_index=False)
